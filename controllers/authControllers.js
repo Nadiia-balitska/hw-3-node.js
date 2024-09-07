9276332a92dc42902fd063df4eed279ea3db1c59
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2024-09-07 22:16:57 +0200

    add gravatar

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,30 +1,49 @@
+import gravatar from "gravatar";
+import fs from "fs/promises";
+import path from "path";
+import Jimp from "jimp";
 import * as authServices from "../services/authServices.js";
-
+import HttpError from "../helpers/HttpError.js";
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
 
+const avatarsPath = path.resolve("public", "covers");
+
 const signup = async (req, res) => {
-  const newUser = await authServices.signup(req.body);
+  const { email, password, username } = req.body;
+  const avatarURL = gravatar.url(email, { s: "250", d: "identicon" }, true);
+  const newUser = await authServices.signup({
+    email,
+    password,
+    username,
+    avatarURL,
+  });
 
   res.status(201).json({
     username: newUser.username,
     email: newUser.email,
+    avatarURL: newUser.avatarURL,
   });
 };
 
 const signin = async (req, res) => {
-  const { token } = await authServices.signin(req.body);
+  const { token, user } = await authServices.signin(req.body);
 
   res.json({
     token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
   });
 };
 
 const getCurrent = (req, res) => {
-  const { username, email } = req.user;
+  const { username, email, avatarURL } = req.user;
 
   res.json({
     username,
     email,
+    avatarURL,
   });
 };
 
